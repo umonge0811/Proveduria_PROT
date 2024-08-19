dd1a6e16bd5c0e56d6909260aa62a1f8667ac83f
umongegds@gmail.com <umongegds@gmail.com>
2024-08-19 17:50:55 -0600

    Editar usuario, mensajes emergentes de Cambio de contraseña y de Eliminacion, navegacion de admin lista

--- Proveduria_PROT/Authentication/UserAccountService.cs
+++ Proveduria_PROT/Authentication/UserAccountService.cs
@@ -9,7 +9,7 @@
             _users = new List<UserAccount>
     {
                 new UserAccount { UserName = "Admin", Password = "admin", Role = "Administrador", Name="Ulises Monge", ProfileImagePath = "img/admin.jpg" },
-                new UserAccount { UserName = "Compras1", Password = "comp123", Role = "Comprador", Name = "Kevin Núñez", ProfileImagePath = "img/ses2.jpg" },
+                new UserAccount { UserName = "Compras1", Password = "comp123", Role = "Comprador", Name = "Kevin Núñez", ProfileImagePath = "img/comp1.jpg" },
                 new UserAccount { UserName = "Jefe", Password = "jefe123", Role = "Jefatura" ,Name = "Elías Méndez", ProfileImagePath = "img/jefe.jpg" },
                 new UserAccount { UserName = "Aprobador1", Password = "Aprob123", Role = "Aprobador", Name = "Dayana Murillo", ProfileImagePath = "img/aprobador1.jpg" }
     };
