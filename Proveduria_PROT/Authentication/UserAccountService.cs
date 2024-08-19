793951615fe4428a56f658e9606ce034647492f5
umongegds@gmail.com <umongegds@gmail.com>
2024-08-19 13:04:33 -0600

    Creado inicio de Administrador

--- Proveduria_PROT/Authentication/UserAccountService.cs
+++ Proveduria_PROT/Authentication/UserAccountService.cs
@@ -9,7 +9,7 @@
             _users = new List<UserAccount>
             {
                 new UserAccount { UserName = "Admin", Password = "admin", Role = "Administrador", Name="Ulises Monge"},
-                new UserAccount { UserName = "Compras1", Password = "comp123", Role = "Comprador", Name = "kevin Núñez"},
+                new UserAccount { UserName = "Compras1", Password = "comp123", Role = "Comprador", Name = "Kevin Núñez"},
                 new UserAccount { UserName = "Jefe", Password = "jefe123", Role = "Jefatura" ,Name = "Elías Méndez" },
                 new UserAccount { UserName = "Aprobador1", Password = "Aprob123", Role = "Aprobador", Name = "Dayana Murillo" }
 
