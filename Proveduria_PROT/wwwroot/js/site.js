4c53d39f7482d0925b7ea1c6111d585f901b23d3
umongegds@gmail.com <umongegds@gmail.com>
2024-08-20 10:13:05 -0600

    Reportes Comprador listo, se visualiza y descarga un PDF demo, icono de login Nuevo

---
+++ Proveduria_PROT/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿function downloadFile(url) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = url.split('/').pop();  // Extrae el nombre del archivo
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
