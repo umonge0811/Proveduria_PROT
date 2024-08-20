19a58895cb02a032228fd24083bfe5650ff1eb64
umongegds@gmail.com <umongegds@gmail.com>
2024-08-20 11:12:36 -0600

    Listo Inicio Jefedep, RevOrdenesJefe

--- Proveduria_PROT/Authentication/UserAccountService.cs
+++ Proveduria_PROT/Authentication/UserAccountService.cs
@@ -10,7 +10,7 @@
     {
                 new UserAccount { UserName = "Admin", Password = "admin", Role = "Administrador", Name="Ulises Monge", ProfileImagePath = "img/admin.jpg" },
                 new UserAccount { UserName = "Compras1", Password = "comp123", Role = "Comprador", Name = "Kevin Núñez", ProfileImagePath = "img/comp1.jpg" },
-                new UserAccount { UserName = "Jefe", Password = "jefe123", Role = "Jefatura" ,Name = "Elías Méndez", ProfileImagePath = "img/jefe.jpg" },
+                new UserAccount { UserName = "1", Password = "1", Role = "Jefe Departamento" ,Name = "Elías Méndez", ProfileImagePath = "img/jefe.jpg" },
                 new UserAccount { UserName = "Aprobador1", Password = "Aprob123", Role = "Aprobador", Name = "Dayana Murillo", ProfileImagePath = "img/aprobador1.jpg" }
     };
         }
